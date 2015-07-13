910cce5e6132d40f41a743b3b34c35ad6f65265e
Sven Engstr?m <development@zw3n.se>
2015-07-13 21:35:30 +0000

    Rename E2303 -> e2303

--- device_e2303.mk
+++ device_e2303.mk
@@ -2,8 +2,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # The gps config appropriate for this device
 $(call inherit-product, device/common/gps/gps_us_supl.mk)
-
-$(call inherit-product-if-exists, vendor/sony/e2303/e2303-vendor.mk)
+# Vendor directory doesnt exist
+#$(call inherit-product-if-exists, vendor/sony/e2303/e2303-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/sony/e2303/overlay
 
